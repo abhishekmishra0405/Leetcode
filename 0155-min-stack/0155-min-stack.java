c9041c2d932e2501069275c63a0871985aa5fddf
abhishekmishra0405 <rnmishramishra973@gmail.com>
2026-09-09 11:34:50 +0530

    Time: 36 ms (25.13%), Space: 99.2 MB (98%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,31 @@
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+    }
+
+    public void push(int val) {
+        st.push(val);
+
+        if (minSt.size() == 0 || val < minSt.peek())
+            minSt.push(val);
+        else
+            minSt.push(minSt.peek());
+    }
+
+    public void pop() {
+        st.pop();
+        minSt.pop();
+    }
+
+    public int top() { // peek
+        return st.peek();
+    }
+
+    public int getMin() {
+        return minSt.peek();
+    }
+}
